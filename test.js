3e12f2ac9e5999fba6eed89bcf42f498fb74ee6b
pemrouz <p3pedram@hotmail.com>
2015-12-10 20:42:15 +0000

    update test

--- test.js
+++ test.js
@@ -4,7 +4,13 @@ var expect = require('chai').expect
 describe('sort', function() {
 
   it('should sort array', function() {
-    expect(sort(Boolean)([0,1,0,2])).to.be.eql([0,0,2,1])
+    expect(sort(az)([0,1,0,2])).to.be.eql([0,0,1,2])
+
+    function az(a, b) {
+      return a > b ?  1
+           : a < b ? -1
+                   :  0
+    }
   })
 
 })
